502b277b54e740e1b2c7b591619048031fba1071
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-13 19:12:29 +0300

    changes

--- laf14-1/Program.cs
+++ laf14-1/Program.cs
@@ -10,6 +10,26 @@ namespace laf14_1
     {
         static void Main(string[] args)
         {
+            Graph theGraph = new Graph();
+            theGraph.addVertex('A'); // 0 
+            theGraph.addVertex('B'); // 1
+            theGraph.addVertex('C'); // 2
+            theGraph.addVertex('D'); // 3
+            theGraph.addVertex('E'); // 4
+
+            theGraph.addEdge(0, 1, 50); // AB 50
+            theGraph.addEdge(0, 3, 80); // AD 80
+            theGraph.addEdge(1, 2, 60); // BC 60
+            theGraph.addEdge(1, 3, 90); // BD 90
+            theGraph.addEdge(2, 4, 40); // CE 40
+            theGraph.addEdge(3, 2, 20); // DC 20
+            theGraph.addEdge(3, 4, 70); // DE 70
+            theGraph.addEdge(4, 1, 50); // EB 50
+
+            Console.WriteLine("Shortest paths");
+            theGraph.path();
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
---
+++ laf14-1/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laf14_1
+{
+    class Vertex
+    {
+        public char lable;
+        public bool isInTree;
+        public Vertex(char lb)
+        {
+            lable = lb;
+            isInTree = false;
+        }
+    }
+}
